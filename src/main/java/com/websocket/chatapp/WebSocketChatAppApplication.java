0777cb11bd232a8fe643c0bfc9554b2f56654021
Issames <lamchabragissame.112@gmail.com>
2023-02-01 13:53:21 +0100

    Initialization

---
+++ src/main/java/com/websocket/chatapp/WebSocketChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.websocket.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketChatAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/websocket/chatapp/WebSocketChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocket.chatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
